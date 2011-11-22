0a2bdef9b50a63c673579ee522e38f540fc13294
Aaron Schmitz <me@aaronschmitz.com>
2011-11-21 22:18:07 -0600

    Switch from pull to fetch to maintain compatibility.

--- pull.php
+++ pull.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-passthru('git pull origin master');
-=======
-passthru('git pull');
->>>>>>> 24055cc28b5f65e47a8008813c33d1184e4fd5ac
+passthru('git fetch origin master');
 passthru('git submodule update --recursive');
 ?>
